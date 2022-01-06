54354759ee750721cff270ed5e60eb65b1d51d4e
BrendaBarrios <brenda.yasmin.barrios@gmail.com>
2022-01-05 19:11:10 -0600

    Adding structure of html for the images

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@
  * When you're ready to start on your site, clear the file. Happy hacking!
  **/
 
+
+// 1. HTML
+// https://randomfox.ca/?i=89
+// 2. imagenes html -> JS
+// 3. Eventos  - DOM
 console.log('Happy hacking :)')
