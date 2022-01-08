b13b97bd304bbbb5aaaf156952fbc62bebbdd17d
yass-brenda <yass-brenda@users.noreply.github.com>
2022-01-08 04:39:59 +0000

    deploy: d7e5684a0f0adaf06a2b4dc981b6e39777690d23

--- _dist_/index.js
+++ _dist_/index.js
@@ -5,7 +5,36 @@
 
 
 // 1. HTML
-// https://randomfox.ca/?i=89
+//
 // 2. imagenes html -> JS
 // 3. Eventos  - DOM
-console.log('Happy hacking :)')
+
+const minimun = 1;
+const maximun = 122;
+
+const random = () => Math.floor(Math.random() * (maximun - minimun)) + minimun
+
+const createImageNode = () => {
+    const  container = document.createElement('div')
+    container.className = "p-4"
+
+    const imagen = document.createElement('img')
+    imagen. className = "mx-auto";
+    imagen.width = '320';
+    imagen.src = `https://randomfox.ca/images/${random()}.jpg`; // TODO
+
+    container.appendChild(imagen)
+
+    return container;
+}
+
+const nuevaImagen = createImageNode()
+const mountNode = document.getElementById("images");
+const addButton = document.querySelector("button");
+
+
+const addImage = () => {
+    const newImage = createImageNode();
+    mountNode.append(newImage)
+}
+addButton.addEventListener("click",addImage)
